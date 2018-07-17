9a3d2aac36aaa28bf6c7a2db21ebbeb6bdb88c5d
Kayler C <kmcookhorn@gmail.com>
2018-07-17 17:00:35 -0400

    api frame added to js

---
+++ jsnotes.js
@@ -0,0 +1,68 @@
+// https://www.w3schools.com/js/tryit.asp?filename=tryjs_object_create_1
+
+var Trainer = {
+  firstName: 'langly',
+  triningLevel: 28,
+
+
+}
+
+
+// class Trainer {
+//   constructor (trainerName) {
+//       this.trainerName = 'Ash';
+//   }
+// }
+
+Carmander = {
+  hp: ,
+  attack:,
+  defense:,
+  abilities: '',
+
+  }
+}
+
+// +++++++++++++++++++++++++
+
+// class player {
+//   constructor(hp, attack, defense, abilities) {
+//     this.hp = 0;
+//     this.attack = 0;
+//     this.defense = 0;
+//     this.abilities = ''
+//   }
+// }
+//
+// class Pokemon extends player {
+//   constructor(hp, attack, defense, abilities) {
+//     super();
+//     this.name = name;
+//     this.hp = 0;
+//     this.attack = 0;
+//     this.defense = 0;
+//     this.abilities = ''
+//   }
+// }
+
+++++++++++++++++++
+//
+//
+// class Charmander extends Pokemon {
+//   constructor(hp, attack, defense, abilities) {
+//     super();
+//     this.hp = 0;
+//     this.attack = 0;
+//     this.defense = 0;
+//     this.abilities = ''
+//   }
+// }
+// class Jigglypuff extends Pokemon {
+//   constructor(hp, attack, defense, abilities) {
+//     super();
+//     this.hp = 0;
+//     this.attack = 0;
+//     this.defense = 0;
+//     this.abilities = ''
+//   }
+// }
