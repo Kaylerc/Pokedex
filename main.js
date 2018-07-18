6ff391e42e691b402a193b0650256eae0197eff5
Kayler C <kmcookhorn@gmail.com>
2018-07-17 20:27:25 -0400

    got ability to push to htmlgit add .git add .!

--- main.js
+++ main.js
@@ -1,11 +1,28 @@
-function loadDoc() {
-  var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function() {
+// function loadDoc() {
+//   var xhttp = new XMLHttpRequest();
+//   xhttp.onreadystatechange = function() {
+//     if (this.readyState == 4 && this.status == 200) {
+//       document.getElementById("info").innerHTML =
+//       this.responseText;
+//     }
+//   };
+//   xhttp.open("GET", "https://pokeapi.co/api/v2/pokemon/1/", true);
+//   xhttp.send();
+// }
+
+
+function loadPokemon() {
+  var newPoke = new XMLHttpRequest();
+  newPoke.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-      document.getElementById("info").innerHTML =
-      this.responseText;
+      var pokedata = JSON.parse(this.responseText);
+      document.getElementById("name").innerHTML = pokedata.name;
+      document.getElementById("hp").innerHTML = pokedata.hp;
+      document.getElementById("attack").innerHTML = pokedata.attack;
+      document.getElementById("defense").innerHTML = pokedata.defense;
+      document.getElementById("abilities").innerHTML = pokedata.abilities[0].ability.name;
     }
   };
-  xhttp.open("GET", "https://raw.githubusercontent.com/Kaylerc/Pokedex/master/testapi/Bulbasaur.js", true);
-  xhttp.send();
+  newPoke.open("GET", "https://pokeapi.co/api/v2/pokemon/1/", true);
+  newPoke.send();
 }
