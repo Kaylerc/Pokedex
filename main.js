925be37474c4024f2fe64a0d5920b6188779997e
Kayler C <kmcookhorn@gmail.com>
2018-07-18 16:54:31 -0400

    js push before object updates

--- main.js
+++ main.js
@@ -1,38 +1,55 @@
 
 
-// allPokemon = [pokemon1, pokemon2, pokemon3 ];
+allPokemon = [];
 
 var trainer = {
-  firstName: 'langly',
+  firstName: 'Langly',
   battlesWon: 1839,
   walkingDistance: 1505,
   pokemonCaught: 663,
   pokemonEvolved: 278,
   eggsHatched: 580,
-  pokestopsVisited: 1677,
   berriesfed: 3093,
-  triningLevel: 28
+  trainingLevel: 28,
+  // pokemon: allPokemon,
+  // getPokemon: function() {
+  //   return this.pokemon;
+  // }
+  // p1: pokemon1 = {
+  //
+  // }
   // all: function() {
   //   return allPokemon;
   // }
 }
-
-
+//
+//
+// function createPokemon(---,----) {
+//   var pokemon = {
+//     name: name,
+//     hp: email
+//   }
+//   totalallPokemon++;
+//   allPokemon.push(pokemon);
+// }
+//
 
 
 function loadPokemon1() {
   var newPoke = new XMLHttpRequest();
   newPoke.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-      var pokemon1 = JSON.parse(this.responseText);
-      name: document.getElementById("name").innerHTML = pokemon1.name;
-      hp: document.getElementById("hp").innerHTML = pokemon1.stats[5].base_stat;
-      attack: document.getElementById("attack").innerHTML = pokemon1.stats[4].base_stat;
-      defense: document.getElementById("defense").innerHTML = pokemon1.stats[3].base_stat;
-      abilities: document.getElementById("abilities").innerHTML = pokemon1.abilities[0].ability.name;
-      abilities: document.getElementById("abilities2").innerHTML = pokemon1.abilities[1].ability.name;
-      console.log(pokemon1);
+      var pokedata = JSON.parse(this.responseText);
+      name: document.getElementById("name").innerHTML = pokedata.name;
+      hp: document.getElementById("hp").innerHTML = pokedata.stats[5].base_stat;
+      attack: document.getElementById("attack").innerHTML = pokedata.stats[4].base_stat;
+      defense: document.getElementById("defense").innerHTML = pokedata.stats[3].base_stat;
+      abilities: document.getElementById("abilities").innerHTML = pokedata.abilities[0].ability.name;
+      abilities2: document.getElementById("abilities2").innerHTML = pokedata.abilities[1].ability.name;
+      console.log(pokedata);
     }
+    // totalallPokemon++;
+    // allPokemon.push(pokemon);
   };
   newPoke.open("GET", "https://pokeapi-nycda.firebaseio.com/pokemon/1.json", true)
   // newPoke.open("GET", "https://pokeapi.co/api/v2/pokemon/1/", true);
